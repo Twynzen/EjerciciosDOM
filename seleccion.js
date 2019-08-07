57d5c5cc578cc157c757762718a03ff578f74d26
Raziel <razzziel001@gmail.com>
2019-08-07 13:43:06 -0500

    Arregle error con metodo style pues utilizaba como metodo la clase item para la variable elemento, agregué funcionalidad al boton que manejaba los li

--- seleccion.js
+++ seleccion.js
@@ -8,9 +8,30 @@ function inicializacion(){
   var cambiarClaseButton = document.getElementById('cambiarClase');
   cambiarClaseButton.addEventListener('click', cambiarEstilosClase);
 
+  var cambiarLiButton = document.getElementById('cambiarLi');
+  cambiarLiButton.addEventListener('click', cambiarEstilosLi);
+  }
+
+
 //esta es la funcion que activa el boton, llama los items
   function cambiarEstilosClase(){
-    var items = document.getElementsByClassName('item');
-    console.log(items);
+    var elemento = document.getElementsByClassName('item');
+    console.log(elemento);
+    console.log(elemento.length);
+    console.log(elemento.item[0]);
+    for (var i = 0; i < elemento.length ; i++) {
+      console.log('item', elemento.item(i));
+      elemento[i].style.color = '#399';
+    }
   }
+//Esta es la funcion que activa el boton y llama los li
+    function cambiarEstilosLi(){
+      var lielemento = document.getElementsByTagName('li');
+      console.log(lielemento);
+
+      for (var i = 0; i < lielemento.length ; i++) {
+        console.log('item', lielemento.item(i));
+        lielemento[i].classList.add('itemgrande');
+      }
+
 }
