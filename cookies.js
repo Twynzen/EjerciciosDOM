6089501b7473d64dbb58f0691c315be22072f5b6
Raziel <razzziel001@gmail.com>
2019-08-11 17:37:40 -0500

    Borrar elementos con el DOM se utiliza el metodo remove

--- cookies.js
+++ cookies.js
@@ -4,5 +4,11 @@ function launchCookieAdvisor() {
   var capaCookies = document.createElement('div');
   capaCookies.innerHTML = '<p>Tienes que aceptar las cookies <span id= "botonaceptar">Aceptar</span></p>';
   capaCookies.classList.add('avisoCookies');
+
+  var todosSpanEnDiv = capaCookies.getElementsByTagName('span');
+  todosSpanEnDiv.item(0).addEventListener('click', function() {
+    //borra elementos el remove
+    capaCookies.remove();
+  });
   document.body.appendChild(capaCookies);
 }
